3bbffc22724f6ebd63ce6699b92ad521983f6a78
Roman Panchuk <panchuk.roman98@gmail.com>
2025-09-06 19:39:56 +0200

    Fixed movieService

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -7,7 +7,10 @@ export type MovieHttpResponse = {
   total_pages: number;
 };
 
-export default async function fetchMovies(query: string, page: number) {
+export default async function fetchMovies(
+  query: string,
+  page: number
+): Promise<MovieHttpResponse> {
   const response = await axios.get<MovieHttpResponse>(
     "https://api.themoviedb.org/3/search/movie",
     {
